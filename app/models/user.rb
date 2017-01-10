cda3cc219cf63d2bec0ee9770ac9cd6baafc11e7
Neel <indraneel.more@gmail.com>
2017-01-10 13:22:35 +0530

    adds username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-z0-9]+\z/i, message: "Username should contain only alphabet and numbers." }
 end
