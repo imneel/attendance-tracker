0fe7be4a4261dfc8887e82d15d0efefeec920c6d
Neel <indraneel.more@gmail.com>
2017-01-10 15:47:26 +0530

    changes to use database_url

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,29 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
+class ActiveRecordOverrideRailtie < Rails::Railtie
+  initializer "active_record.initialize_database.override" do |app|
+
+    ActiveSupport.on_load(:active_record) do
+      if url = ENV['DATABASE_URL']
+        ActiveRecord::Base.connection_pool.disconnect!
+        parsed_url = URI.parse(url)
+        config =  {
+          adapter:             'postgis',
+          host:                parsed_url.host,
+          encoding:            'unicode',
+          database:            parsed_url.path.split("/")[-1],
+          port:                parsed_url.port,
+          username:            parsed_url.user,
+          password:            parsed_url.password
+        }
+        establish_connection(config)
+      end
+    end
+  end
+end
+
 Bundler.require(*Rails.groups)
 
 module GeniusUTest
